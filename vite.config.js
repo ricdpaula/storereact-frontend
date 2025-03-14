517700ba57d129471258f51c49c6ff79e91a273c
ricpaula <cadodepaulasilva159@gmail.com>
2025-03-14 16:24:46 -0300

    Update deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build:{
+    target: "esnext"
+  },
   plugins: [react(), tailwindcss()],
 })
